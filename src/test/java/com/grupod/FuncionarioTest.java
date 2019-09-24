bb940d24d4c166af38699f74c0c226f8bb0ac5d2
Doyle Mello Barboza <doylemb@hotmail.com>
2019-09-24 11:24:29 -0300

    initial config

--- src/test/java/com/grupod/FuncionarioTest.java
+++ src/test/java/com/grupod/FuncionarioTest.java
@@ -1,20 +1,18 @@
 package com.grupod;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class FuncionarioTest 
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void getSalarioBruto()
     {
-        assertTrue( true );
+        assert( true );
     }
 }
